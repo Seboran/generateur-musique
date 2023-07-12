ffee451f210423055f544ae264cae2704f54bbe9
Nirina Rabeson <nirina.rabeson@zenika.com>
2023-07-12 16:28:31 +0200

    exemple fonctionnel de résolution de sudoku par propagation

---
+++ packages/visualisation/components/BoutonPropagation.vue
@@ -0,0 +1,17 @@
+<template>
+  <button
+    class="bg-blue-500 hover:bg-blue-600 active:bg-blue-700 text-white font-bold py-2 px-4 rounded m-4 transition-transform duration-10 ease-in-out transform active:translate-y-1 active:translate-x-1"
+    type="button"
+    @click="$emit('click')"
+  >
+    Propager
+  </button>
+</template>
+
+<script setup lang="ts">
+defineEmits<{
+  click: []
+}>()
+</script>
+
+<style scoped></style>
--- packages/visualisation/pages/sudoku.vue
+++ packages/visualisation/pages/sudoku.vue
@@ -1,20 +1,22 @@
 <template>
-  <div><AfficherGrilleSudoku :grid="sudoku.grid" /></div>
-  <button type="button" @click="propager">Propager</button>
+  <div>
+    <div><AfficherGrilleSudoku :grid="sudoku.grid" /></div>
+    <BoutonPropagation @click="propager" />
+  </div>
 </template>
 
 <script setup lang="ts">
 import { Sudoku, SudokuGrid } from 'effondrement'
 const initialSudokuGrid: SudokuGrid = [
-  [0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [2, 0, 0, 0, 0, 0, 0, 0, 0],
-  [3, 0, 5, 0, 0, 0, 0, 0, 0],
-  [4, 0, 0, 0, 0, 0, 0, 0, 0],
-  [5, 0, 0, 0, 0, 0, 8, 0, 0],
-  [6, 0, 0, 3, 0, 0, 0, 0, 0],
-  [7, 0, 5, 0, 0, 0, 0, 0, 0],
-  [8, 1, 4, 0, 0, 0, 0, 0, 0],
-  [9, 2, 3, 0, 1, 0, 0, 0, 0],
+  [0, 0, 0, 0, 7, 0, 0, 8, 0],
+  [2, 7, 4, 9, 0, 8, 0, 0, 5],
+  [0, 0, 5, 0, 1, 0, 2, 7, 0],
+  [1, 0, 0, 4, 0, 0, 0, 6, 7],
+  [0, 0, 2, 0, 8, 0, 0, 5, 4],
+  [7, 4, 0, 5, 0, 0, 9, 0, 0],
+  [5, 0, 9, 1, 4, 0, 0, 0, 8],
+  [3, 0, 1, 8, 9, 0, 0, 4, 2],
+  [0, 8, 0, 3, 0, 2, 1, 9, 6],
 ]
 const initialSudoku = new Sudoku()
 initialSudoku.initialize(initialSudokuGrid)
