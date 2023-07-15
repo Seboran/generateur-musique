fb512f4fa1a6cb86cfb5e16fe2a26506f3cc838e
Nirina Rabeson <nirina.rabeson@zenika.com>
2023-07-15 12:44:15 +0200

    refactor musique sheet

--- packages/visualisation/components/musique/MusiqueSheet.vue
+++ packages/visualisation/components/musique/MusiqueSheet.vue
@@ -5,6 +5,7 @@
 <script setup lang="ts">
 import { SudokuCellule, SudokuGrid } from 'effondrement'
 import { OpenSheetMusicDisplay } from 'opensheetmusicdisplay'
+import { createPartition } from './musiqueUtils'
 
 let osmd: any
 
@@ -29,64 +30,15 @@ watchEffect(
 
     // Convert the chord to music notes and format them for OSMD
     const notes = props.grilleAccords[0]!.map(numberToNote)
-    const xmlString = createPartition()
+    const xmlString = createPartition(notes)
 
     await osmd.load(xmlString)
     osmd.render()
-
-    function createPartition() {
-      return `
-  <?xml version="1.0" encoding="UTF-8"?>
-<!DOCTYPE score-partwise PUBLIC "-//Recordare//DTD MusicXML 3.1 Partwise//EN" "http://www.musicxml.org/dtds/partwise.dtd">
-<score-partwise version="3.1">
-  <work>
-    <work-title>Titre</work-title>
-  </work>
-  <part-list>
-    <score-part id="P1">
-      <part-name>Music</part-name>
-    </score-part>
-  </part-list>
-  <part id="P1">
-    ${getMesures(notes)}
-  </part>
-</score-partwise>`
-    }
   },
   {
     flush: 'post',
   }
 )
-
-function getMesures(notes: string[]) {
-  // split notes into measures of 4 notes
-  const measures = []
-  for (let i = 0; i < notes.length; i += 4) {
-    measures.push(notes.slice(i, i + 4))
-  }
-
-  console.log(measures)
-
-  return measures
-    .map(
-      (measure, index) => `<measure number="${index + 1}">
-      ${measure
-        .map(
-          (note) => `
-        <note>
-          <pitch>
-            <step>${note[0]}</step>
-            <octave>4</octave>
-          </pitch>
-          <duration>1</duration>
-          <type>quarter</type>
-        </note>`
-        )
-        .join('')}
-    </measure>`
-    )
-    .join('')
-}
 </script>
 
 <style scoped>
