46fefef86f6074727201c011b0ff53f9c62a6950
Nirina Rabeson <nirina.rabeson@zenika.com>
2023-08-15 19:31:47 +0200

    style(sudoku): mise de la couleur jaune !

--- packages/app-visualisation/components/styled/StyledBouton.vue
+++ packages/app-visualisation/components/styled/StyledBouton.vue
@@ -1,6 +1,6 @@
 <template>
   <button
-    class="duration-10 m-4 transform rounded bg-blue-500 px-4 py-2 font-bold text-white transition-transform ease-in-out hover:bg-blue-600 active:translate-x-1 active:translate-y-1 active:bg-blue-700"
+    class="duration-10 bg-citrine-500 hover:bg-citrine-600 active:bg-citrine-700 m-4 transform rounded px-4 py-2 font-bold text-white transition-transform ease-in-out active:translate-x-1 active:translate-y-1"
     type="button"
     @click="$emit('click')"
   >
